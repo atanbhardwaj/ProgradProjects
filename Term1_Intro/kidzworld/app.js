975dba47286431f805020bbb428fce7e0d28a950
Atan Bhardwaj <atanbhardwaj@gmail.com>
2021-07-29 00:44:23 +0530

    js completed

--- Term1_Intro/kidzworld/app.js
+++ Term1_Intro/kidzworld/app.js
@@ -1,5 +1,4 @@
 var cartValue = document.getElementById("cart-value");
-
 var cartButton = document.getElementById("cart");
 
 var book1Add = document.getElementById("book1");
@@ -14,7 +13,6 @@ var game2Add = document.getElementById("game2");
 var game3Add = document.getElementById("game3");
 var game4Add = document.getElementById("game4");
 
-
 var craft1Add = document.getElementById("craft1");
 var craft2Add = document.getElementById("craft2");
 var craft3Add = document.getElementById("craft3");
@@ -24,96 +22,100 @@ var craft4Add = document.getElementById("craft4");
 var book1 = {
     name: "This was our pact",
     quantity: 0,
-    dollar: 7,
+    dollars: 7,
     cents: 49,
 };
+
+
 var book2 = {
     name: "The famous five",
     quantity: 0,
-    dollar: 4,
+    dollars: 4,
     cents: 59,
 };
 var book3 = {
     name: "Matlida",
     quantity: 0,
-    dollar: 6,
+    dollars: 6,
     cents: 80,
 };
 var book4 = {
     name: "Harry Potter",
     quantity: 0,
-    dollar: 10,
+    dollars: 10,
     cents: 0,
 };
 var book5 = {
     name: "For Young Readers",
     quantity: 0,
-    dollar: 7,
+    dollars: 7,
     cents: 29,
 };
 var book6 = {
-    name: "Wimpy Kid- DIY",
+    name: "Wimpy Kid - DIY",
     quantity: 0,
-    dollar: 4,
+    dollars: 4,
     cents: 99,
 };
-
-
 
 var game1 = {
     name: "Dart Board",
     quantity: 0,
-    dollar: 17,
+    dollars: 17,
     cents: 49,
 };
+
 var game2 = {
     name: "Connect 4",
     quantity: 0,
-    dollar: 19,
+    dollars: 19,
     cents: 99,
 };
+
 var game3 = {
     name: "Jenga",
     quantity: 0,
-    dollar: 20,
+    dollars: 20,
     cents: 99,
 };
+
 var game4 = {
     name: "Monopoly",
     quantity: 0,
-    dollar: 19,
+    dollars: 19,
     cents: 49,
 };
-
-
 
 var craft1 = {
     name: "Bookmarks",
     quantity: 0,
-    dollar: 12,
+    dollars: 12,
     cents: 49,
 };
+
 var craft2 = {
     name: "Birthday card",
     quantity: 0,
-    dollar: 19,
+    dollars: 19,
     cents: 99,
 };
+
 var craft3 = {
     name: "Stuffed toys",
     quantity: 0,
-    dollar: 15,
+    dollars: 15,
     cents: 99,
 };
+
 var craft4 = {
     name: "Dream catcher drawing",
     quantity: 0,
-    dollar: 18,
+    dollars: 18,
     cents: 49,
 };
 
 function updateCart() {
-    var cart =
+    cart =
         book1.quantity +
         book2.quantity +
         book3.quantity +
@@ -128,12 +130,136 @@ function updateCart() {
         craft2.quantity +
         craft3.quantity +
         craft4.quantity;
-
     cartValue.innerHTML = cart;
 }
 
-var whatsappLink = "https://api.whatsapp.com/send?phone=917985095357&text=Order%20details";
+book1Add.onclick = (e) => {
+    book1.quantity++;
+    updateCart();
+};
 
+book2Add.onclick = (e) => {
+    book2.quantity++;
+    updateCart();
+};
+
+book3Add.onclick = (e) => {
+    book3.quantity++;
+    updateCart();
+};
+
+book4Add.onclick = (e) => {
+    book4.quantity++;
+    updateCart();
+};
+
+book5Add.onclick = (e) => {
+    book5.quantity++;
+    updateCart();
+};
+
+book6Add.onclick = (e) => {
+    book6.quantity++;
+    updateCart();
+};
+
+game1Add.onclick = (e) => {
+    game1.quantity++;
+    updateCart();
+};
+
+game2Add.onclick = (e) => {
+    game2.quantity++;
+    updateCart();
+};
+
+game2Add.onclick = (e) => {
+    game2.quantity++;
+    updateCart();
+};
+
+game4Add.onclick = (e) => {
+    game4.quantity++;
+    updateCart();
+};
+
+craft1Add.onclick = (e) => {
+    craft1.quantity++;
+    updateCart();
+};
+
+craft2Add.onclick = (e) => {
+    craft2.quantity++;
+    updateCart();
+};
+
+craft3Add.onclick = (e) => {
+    craft3.quantity++;
+    updateCart();
+};
+
+craft4Add.onclick = (e) => {
+    craft4.quantity++;
+    updateCart();
+};
+
+var finalDollars = 0;
+var finalcents = 0;
+
+function updatePrice() {
+    var book1CostInCents =
+        book1.quantity * book1.dollars * 100 + book1.quantity * book1.cents;
+    var book2CostInCents =
+        book2.quantity * book2.dollars * 100 + book2.quantity * book2.cents;
+    var book3CostInCents =
+        book3.quantity * book3.dollars * 100 + book3.quantity * book3.cents;
+    var book4CostInCents =
+        book4.quantity * book4.dollars * 100 + book4.quantity * book4.cents;
+    var book5CostInCents =
+        book5.quantity * book5.dollars * 100 + book5.quantity * book5.cents;
+    var book6CostInCents =
+        book6.quantity * book6.dollars * 100 + book6.quantity * book6.cents;
+
+    var game1CostInCents =
+        game1.quantity * game1.dollars * 100 + game1.quantity * game1.cents;
+    var game2CostInCents =
+        game2.quantity * game2.dollars * 100 + game2.quantity * game2.cents;
+    var game3CostInCents =
+        game3.quantity * game3.dollars * 100 + game3.quantity * game3.cents;
+    var game4CostInCents =
+        game4.quantity * game4.dollars * 100 + game4.quantity * game4.cents;
+
+    var craft1CostInCents =
+        craft1.quantity * craft1.dollars * 100 + craft1.quantity * craft1.cents;
+    var craft2CostInCents =
+        craft2.quantity * craft2.dollars * 100 + craft2.quantity * craft2.cents;
+    var craft3CostInCents =
+        craft3.quantity * craft3.dollars * 100 + craft3.quantity * craft3.cents;
+    var craft4CostInCents =
+        craft4.quantity * craft4.dollars * 100 + craft4.quantity * craft4.cents;
+
+    totalPriceInCents =
+        book1CostInCents +
+        book2CostInCents +
+        book3CostInCents +
+        book4CostInCents +
+        book5CostInCents +
+        book6CostInCents +
+        game1CostInCents +
+        game2CostInCents +
+        game3CostInCents +
+        game4CostInCents +
+        craft1CostInCents +
+        craft2CostInCents +
+        craft3CostInCents +
+        craft4CostInCents;
+
+    finalDollars = Math.floor(totalPriceInCents / 100);
+    finalCents = totalPriceInCents % 100;
+}
+
+var whatsappLink =
+    "https://api.whatsapp.com/send?phone=919000000000&text=Order%20details";
 
 function updateWhatsappLink() {
     if (book1.quantity != 0) {
@@ -154,6 +280,7 @@ function updateWhatsappLink() {
     if (book6.quantity != 0) {
         whatsappLink += "%0A" + book6.name + "%20" + book6.quantity;
     }
+
     if (game1.quantity != 0) {
         whatsappLink += "%0A" + game1.name + "%20" + game1.quantity;
     }
@@ -166,6 +293,7 @@ function updateWhatsappLink() {
     if (game4.quantity != 0) {
         whatsappLink += "%0A" + game4.name + "%20" + game4.quantity;
     }
+
     if (craft1.quantity != 0) {
         whatsappLink += "%0A" + craft1.name + "%20" + craft1.quantity;
     }
@@ -178,76 +306,12 @@ function updateWhatsappLink() {
     if (craft4.quantity != 0) {
         whatsappLink += "%0A" + craft4.name + "%20" + craft4.quantity;
     }
-
-    whatsappLink += "%0A" + "Total%20Price:%20$" + finalDollars + "%20" + finalCents + "c";
+    whatsappLink +=
+        "%0A" + "Total%20Price:%20$" + finalDollars + "%20" + finalCents + "c";
 }
 
 cartButton.onclick = () => {
     updatePrice();
     updateWhatsappLink();
     window.open(whatsappLink, "_blank");
-}
-
-
-
-book1Add.onclick = (e) => {
-    book1.quantity++;
-    updateCart();
-};
-book2Add.onclick = (e) => {
-    book2.quantity++;
-    updateCart();
-};
-book3Add.onclick = (e) => {
-    book3.quantity++;
-    updateCart();
-};
-book4Add.onclick = (e) => {
-    book4.quantity++;
-    updateCart();
-};
-book5Add.onclick = (e) => {
-    book5.quantity++;
-    updateCart();
-};
-book6Add.onclick = (e) => {
-    book6.quantity++;
-    updateCart();
-};
-
-
-
-game1Add.onclick = (e) => {
-    game1.quantity++;
-    updateCart();
-};
-game2Add.onclick = (e) => {
-    game2.quantity++;
-    updateCart();
-};
-game3Add.onclick = (e) => {
-    game3.quantity++;
-    updateCart();
-};
-game4Add.onclick = (e) => {
-    game4.quantity++;
-    updateCart();
-};
-
-
-craft1Add.onclick = (e) => {
-    craft1.quantity++;
-    updateCart();
-};
-craft2Add.onclick = (e) => {
-    craft2.quantity++;
-    updateCart();
-};
-craft3Add.onclick = (e) => {
-    craft3.quantity++;
-    updateCart();
-};
-craft4Add.onclick = (e) => {
-    craft4.quantity++;
-    updateCart();
 };
